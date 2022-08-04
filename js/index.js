8778b4f28ee39b1edd6851cccd86a6b105aa04fe
kaitto1425m <delatorreroy.pdm@gmail.com>
2022-08-04 13:42:26 +0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    autoplay:100,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        700:{
+            items:2,
+            nav:false
+        },
+        1000:{
+            items:4,
+            nav:true,
+            loop:false
+        }
+    }
+})
+  });
